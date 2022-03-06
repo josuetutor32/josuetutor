bab31c0f90d6f54d53dc50faccaa1f0e7a9db933
josue tutor <49314838+josuetutor32@users.noreply.github.com>
2022-03-06 12:03:02 +0800

    Update mail.js

--- mail.js
+++ mail.js
@@ -6,23 +6,34 @@ const formMail = document.querySelector("#formMail")
 
 formMail.addEventListener('submit', e => {
     e.preventDefault()
-    let fullname = document.querySelector('#fullname').value
-    let userEmail = document.querySelector('#mail').value
-    let subjectMail = document.querySelector('#subject').value
-    let userMessage = document.querySelector('#message').value
 
-    const contactParams = {
-        from_name: fullname,
-        from_mail: userEmail,
-        subject: subjectMail,
-        message: userMessage,
+    const fullname = document.querySelector('#fullname').value
+    const userEmail = document.querySelector('#mail').value
+    const subjectMail = document.querySelector('#subject').value
+    const userMessage = document.querySelector('#message').value
+
+    if (fullname == null || fullname === '' && userEmail == null || userEmail === '' &&
+        subjectMail == null || subjectMail === '' && userMessage == null || userMessage === '') {
+        return alert('Error!')
+    } else {
+        const contactParams = {
+            from_name: fullname,
+            from_mail: userEmail,
+            subject: subjectMail,
+            message: userMessage,
+        }
+
+        emailjs.send('service_5ij832g', 'template_kc9qkte', contactParams)
+            .then(res => {
+                setTimeout(() => {
+                    alert('success', res.status, res.text)
+                }, 3000)
+
+            }).catch(err => {
+                setTimeout(() => {
+                    alert('error!', err.message)
+                })
+            })
     }
-
-    emailjs.send('service_5ij832g', 'template_kc9qkte', contactParams)
-        .then(function(res) {
-            console.log('success', res.status, res.text)
-        }).catch((err) => {
-            console.log('failed', err)
-        })
 
 })
