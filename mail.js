aa8d2b69cba2238a49cf25fa9810cdea990ed092
Josue Tutor <49314838+josuetutor32@users.noreply.github.com>
2023-10-24 22:11:33 +0800

    Update mail.js

--- mail.js
+++ mail.js
@@ -2,31 +2,31 @@
     emailjs.init("h7g8644w2bHhmPT_Egiq-);
 })();
 
-const formMail = document.querySelector("#formMail")
-const formTitle = document.querySelector(".form-title")
+const formMail = document.querySelector("#formMail");
+const errorMessage = document.querySelector(".error-message");
 
 function message(msg) {
-    return `${msg}`
+    return `${msg}`;
 }
 
 formMail.addEventListener('submit', e => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const fullname = document.querySelector('#fullname').value
-    const userEmail = document.querySelector('#mail').value
-    const subjectMail = document.querySelector('#subject').value
-    const userMessage = document.querySelector('#message').value
+    const fullname = document.querySelector('#fullname').value;
+    const userEmail = document.querySelector('#mail').value;
+    const subjectMail = document.querySelector('#subject').value;
+    const userMessage = document.querySelector('#message').value;
 
     if (!fullname || !userEmail || !subjectMail || !userMessage) {
-        formTitle.textContent = message('Input is required!')
-        return
+        errorMessage.textContent = message('Input is required!');
+        return;
     } else {
         const contactParams = {
             from_name: fullname,
             from_mail: userEmail,
             subject: subjectMail,
             message: userMessage,
-        }
+        };
 
         emailjs.send('service_6v88ywc', 'template_xy03tyj', contactParams)
             .then(res => {
